73a2bf20d89300d29a1cf2f57d9159032e814855
st <st@A510PC3>
2026-02-16 18:39:59 +0300

    new

--- lesson41Solution/task02.cpp
+++ lesson41Solution/task02.cpp
@@ -1,19 +1,24 @@
 #include<iostream>
+#define DEFUALT_SIZE 20
 using namespace std;
 int main() {
-	const int MARK_COUNT = 3;
-	int array[MARK_COUNT];
-	for (int i = 0; i < MARK_COUNT; i++)
+	int array[DEFUALT_SIZE];
+	int length;
+	do {
+		cout << "input number";
+		cin >> length;
+	} while (length <= 0 && length > DEFUALT_SIZE);
+	for (int i = 0; i < length; i++)
 	{
 		cin>>array[i] ;
 	}
 	double avg = 0;
-	for (int index = 0; index < MARK_COUNT; index++)
+	for (int index = 0; index < length; index++)
 	{
 		avg += array[index];
 	}
 
-	cout << "avarage mark is" << avg / 3 << endl;
+	cout << "avarage mark is" << avg / length << endl;
 
 	return 0;
 
