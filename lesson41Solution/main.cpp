7b50c356b77ae59724d4b392dd12b87286e22683
st <st@A510PC3>
2026-02-16 17:41:59 +0300

    Add project files.

---
+++ lesson41Solution/main.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//using namespace std;
+//#define STUDANCE_COUT 3.0
+//
+//double calculate_mark(int a, int b, int c);
+//
+//int main() {
+//	double mark1, mark2, mark3;
+//
+//	cout << "input 3 marks:";
+//	cin >> mark1>> mark2>> mark3;
+//
+//	double result = calculate_mark(mark1, mark2, mark3);
+//
+//	cout << "result-->" << result;
+//
+//	return 0;
+//}
+//double calculate_mark(int a,int b , int c) {
+//	return (a + b + c) / STUDANCE_COUT;
+//}
---
+++ lesson41Solution/test01.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#define DEFAULT_SIZE 10//C notation
+using namespace std;
+
+int main() {
+	/*int numbers[10];
+	int vector[10];*/
+	//1)int length = 10;
+	
+	int array[ DEFAULT_SIZE ];
+	for (int index = 0; index < DEFAULT_SIZE; index++)
+	{
+		array[index] = 0;
+		cout << "[" << index+1 << "]-->" << array[index] << endl;
+	}
+
+
+	return 0;
+}
